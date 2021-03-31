2ceeba30d9a6f9d34f13199bc07b125f0a655ff7
daeyeong1573 <57317043+daeyeong1573@users.noreply.github.com>
2021-03-31 23:03:13 +0900

    [UPDATE] DataBinding 주석추가

--- app/src/main/java/com/example/pratice2/dataBindg/DataBindingActivity.kt
+++ app/src/main/java/com/example/pratice2/dataBindg/DataBindingActivity.kt
@@ -13,17 +13,19 @@ import com.example.pratice2.databinding.ActivityDataBindingBinding
 
 class DataBindingActivity : AppCompatActivity() {
 
-    private lateinit var binding: ActivityDataBindingBinding
 
     var text = "TEST"
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_data_binding)
-        binding = DataBindingUtil.setContentView<ActivityDataBindingBinding>(this,R.layout.activity_data_binding)
-
+        //바인딩 세팅
+       val binding = DataBindingUtil.setContentView<ActivityDataBindingBinding>(this,R.layout.activity_data_binding)
+        // 현재 binding시킨 xml의 variable name
         binding.test = this
         binding.button2.setOnClickListener {
-            text = "TEST2"
+            text = "바인딩 성공"
+            Toast.makeText(this,"바인딩 성공",Toast.LENGTH_SHORT).show()
+            // Data가 변동될 경우 binding된 View들에 Data 변화를 알려줌
             binding.invalidateAll()
         }
     }
